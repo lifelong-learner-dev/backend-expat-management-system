cef5012980dd76512f1b371efadc4c015d23f8be
kaineus2999 <kaineus2999@gmail.com>
2023-06-28 23:12:04 +0300

    serializers.py revision

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class TinyUserSerializer(ModelSerializer):
     class Meta: 
         model = User
-        fields = ("first_name", "last_name", "avatar", "car_plate", "company_car_model", "car_model_year", "is_company_cars", "is_supporter", "is_manager", "is_expat", "is_director", "first_name", "last_name", "cellphone_number", "email")
+        fields = ("username", "avatar", "car_plate", "company_car_model", "car_model_year", "is_company_cars", "is_supporter", "is_manager", "is_expat", "is_director", "first_name", "last_name", "cellphone_number", "email")
 
 
 class PrivateUserSerializer(ModelSerializer):
--- work_permits/serializers.py
+++ work_permits/serializers.py
@@ -18,12 +18,17 @@ class Work_permitDetailSerializer(ModelSerializer):
         return False
 
 class Work_permitListSerializer(ModelSerializer):
+    expat = TinyUserSerializer(read_only=True)
     is_expat = SerializerMethodField()
+    krstatus_display = SerializerMethodField(source='get_krstatus_display', read_only=True)
 
     class Meta:
         model = Work_permit
-        fields = ("pk", "name", "expat", "is_expat", "created_at", "updated_at",)
+        fields = ("pk", "name", "expat", "is_expat", "krstatus", "krstatus_display", "enstatus", "created_at", "updated_at",)
 
     def get_is_expat(self, work_permit):
         request = self.context.get("request")
         return work_permit.expat == request.user
+
+    def get_krstatus_display(self, work_permit):
+        return dict(work_permit.KrstatusChoices.choices).get(work_permit.krstatus, "")
