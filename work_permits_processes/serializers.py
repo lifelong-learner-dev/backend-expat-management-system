495de2fa34d45fca94d5db34ccb81fdb748fb8fa
kaineus2999 <kaineus2999@gmail.com>
2023-07-01 13:25:59 +0300

    Serializers expat work permits processes

--- work_permits_processes/serializers.py
+++ work_permits_processes/serializers.py
@@ -3,13 +3,27 @@ from .models import Work_permits_process
 from users.serializers import TinyUserSerializer
 
 class Work_permits_processDetailSerializer(ModelSerializer):
+    responsible_person = TinyUserSerializer(read_only=True)
+    is_responsible_person = SerializerMethodField()
 
     class Meta:
         model = Work_permits_process
         fields = "__all__"
 
+    def get_is_responsible_person(self, work_permits_process):
+        request = self.context.get("request")
+        if request:
+            return work_permits_process.responsible_person == request.user
+        return False
+
 class Work_permits_processListSerializer(ModelSerializer):
+    responsible_person = TinyUserSerializer(read_only=True)
+    is_responsible_person = SerializerMethodField()
 
     class Meta:
         model = Work_permits_process
-        fields = ("pk", "name", "subject", "responsible_person_name", "is_responsible_person_name", "created_at", "updated_at",)
+        fields = "__all__"
+
+    def get_is_responsible_person(self, work_permits_process):
+        request = self.context.get("request")
+        return work_permits_process.responsible_person == request.user
