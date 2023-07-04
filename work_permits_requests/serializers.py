1151029f833d2ae8152344c611f3c6de9d1539c9
kaineus2999 <kaineus2999@gmail.com>
2023-07-04 23:45:39 +0300

    Serializers

--- work_permits_requests/serializers.py
+++ work_permits_requests/serializers.py
@@ -18,13 +18,19 @@ class Work_permits_requestDetailSerializer(ModelSerializer):
         return False
 
 class Work_permits_requestListSerializer(ModelSerializer):
+    expat = TinyUserSerializer(read_only=True)
 
     is_expat = SerializerMethodField()
+    krstatus_display = SerializerMethodField(source='get_krstatus_display', read_only=True)
+
 
     class Meta:
         model = Work_permits_request
-        fields = ("pk", "name", "expat", "is_expat", "create_at", "updated_at",)
+        fields = ("pk", "name", "expat", "is_expat", "location", "date", "krstatus", "krstatus_display", "created_at", "updated_at",)
 
     def get_is_expat(self, work_permits_request):
         request = self.context.get("request")
         return work_permits_request.expat == request.user
+
+    def get_krstatus_display(self, work_permits_request):
+        return dict(work_permits_request.KrstatusChoices.choices).get(work_permits_request.krstatus, "")
